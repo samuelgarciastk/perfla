8fdc53d92de1490dfd90b8986cbfe58f43a10949
Samuel Garcia <samuelgarciastk@gmail.com>
2018-10-29 19:47:01 +0800

    update pom

--- logger/src/main/scala/io/transwarp/perfla/logger/PerfLogger.scala
+++ logger/src/main/scala/io/transwarp/perfla/logger/PerfLogger.scala
@@ -5,16 +5,11 @@ import org.slf4j.LoggerFactory
 
 class PerfLogger(clazz: Class[_] = classOf[PerfLogger]) {
   private val logger = LoggerFactory.getLogger(clazz)
-  private var isEnable: Boolean = false
+  private var isEnable: Boolean = true
 
   def checkpoint: Checkpoint = {
     val caller = (new Throwable).getStackTrace()(1)
     checkpoint(caller.getClassName, caller.getMethodName, null)
-  }
-
-  def checkpoint(uuid: String): Checkpoint = {
-    val caller = (new Throwable).getStackTrace()(1)
-    checkpoint(caller.getClassName, caller.getMethodName, uuid)
   }
 
   def checkpoint(className: String, methodName: String, uuid: String): Checkpoint = {
@@ -28,9 +23,14 @@ class PerfLogger(clazz: Class[_] = classOf[PerfLogger]) {
     checkpoint
   }
 
+  def checkpoint(uuid: String): Checkpoint = {
+    val caller = (new Throwable).getStackTrace()(1)
+    checkpoint(caller.getClassName, caller.getMethodName, uuid)
+  }
+
   def log(checkpoint: Checkpoint): Unit = {
     checkpoint.endTime = System.currentTimeMillis
-    if (checkpoint.task != null) {
+    if (isEnable && checkpoint.task != null) {
       val diff = checkpoint.endTime - checkpoint.startTime
       diff match {
         case d if d > checkpoint.task.threshold.error * checkpoint.dataSize =>
