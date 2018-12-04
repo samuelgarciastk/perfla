c06ed6b8e06d89acae32c4ab1796c5fc5e7d2fbe
stk <shenghui.gu@transwarp.io>
2018-12-04 10:46:47 +0800

    update build.sh

--- build.sh
+++ build.sh
@@ -4,6 +4,6 @@ set -e
 
 mvn clean install
 
-dir='/home/stk/Documents/perfla'
+dir='/home/stk/Documents/perfla/jars/'
 cp logger/target/perfla-logger-1.0.0-SNAPSHOT.jar $dir
 cp analyzer/target/perfla-analyzer-1.0.0-SNAPSHOT.jar $dir
